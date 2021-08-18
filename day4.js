b08a5c9e0be8426d865fd2bbceb8368b7c550470
GentleToyin <41758364+GentleToyin@users.noreply.github.com>
2021-08-18 14:17:05 +0100

    Added day 4

---
+++ day4.js
@@ -0,0 +1,128 @@
+
+// Exercise: Level One
+// ONE
+/*
+let age = prompt('Enter your age', 'Enter age here');
+let driveAge = 18;
+switch (true){
+    case age>18:
+        console.log ("You are old enough to drive");
+        break;
+    case age ==18:
+        console.log("welcome to the prime driving age")
+        break;
+    case age<18:
+        console.log("you are left with " + (18-age)  + " years before driving")
+    
+}
+*/
+
+//TWO
+
+/*let myAge = prompt('Enter my age');
+let yourAge = prompt('Your age');
+
+if (myAge>yourAge){
+    alert('I am older')
+}
+else if ( myAge<yourAge){
+    alert('You are older')
+}
+else{
+    alert("We are age mate")
+}
+*/
+//THREE
+/*
+let a = prompt('Enter value for A');
+let b = prompt('Enter value for B');
+
+if (a>b){
+    alert('A is greater than B')
+}
+else if ( a<b){
+    alert('B is greater than A')
+}
+else{
+    alert("A and B are equal")
+}
+
+
+ a = prompt('Enter value for A');
+ b = prompt('Enter value for B');
+
+a>b
+?  alert('A is greater than B')
+:  alert('B is greater than A')
+
+*/
+//Four
+/*
+let number = prompt('Enter Number')
+if(number%2===0){
+    alert(number + ' is an even number')
+}else{
+    alert(number + ' is an odd number')
+}
+*/
+// EXERCISES: LEVEL 2
+/*let grade = prompt('Enter your grade', 'Enter grade here');
+
+switch(true){
+    case grade >=80 && grade<=100:
+        console.log("A logba")
+        break;
+    case grade >=70 && grade<=79:
+        console.log("B logba")
+        break;
+    case grade >=60 && grade<=69:
+        console.log("C logba")
+        break;
+    case grade >=50 && grade<=59:
+        console.log("D logba")
+        break;
+        case grade <50:
+            console.log("F logba")
+            break;
+    default: console.log("Input the right score jor! OLODO!")
+    
+
+    
+}
+*/
+// TWO
+
+let userInput = prompt('Enter the month');
+let month = userInput.toLowerCase;
+let autumn = 'september october november';
+let winter = 'december january february'
+let spring = 'march april may'
+let summer = 'june july august'
+
+/*switch(true) {
+    case autumn.includes(month):
+        console.log("Season is Autumn")
+        break;
+    case winter.includes(month):
+        console.log("Season is Winter")
+        break;
+    case spring.includes(month):
+        console.log("Season is Spring")
+        break;
+    case summer.includes(month):
+        console.log("Season is Summer")
+        break;
+}*/
+
+if (autumn.includes(month)){
+    console.log("Season is Autumn")
+}
+else if (winter.includes(month)){
+    console.log("Season is winter")
+}
+else if (spring.includes(month)){
+    console.log("Season is spring")
+}
+else {
+    console.log("Season is summer")
+}
